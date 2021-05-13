9a6938c5614457790b324183c60f8a4dfde70c28
Pramod Gunake <pramodgunake@gmail.com>
2021-05-13 23:09:40 +0530

    Inter Component Communication and Databinding

---
+++ cmp-databinding-start/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements = [{type: 'server', name: 'Pramod Server', content: 'This is a Test Server'},
+  {type: 'blueprint', name: 'Pramod Blue Print', content: 'This is a Blue Print'}];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    console.log("onServerAdded called");
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    console.log("onBlueprintAdded called");
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
+
+
+
+}
