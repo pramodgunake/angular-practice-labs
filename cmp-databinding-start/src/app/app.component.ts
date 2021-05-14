3535b92575d116d421447b81f0e0a0f7cb62f784
Pramod Gunake <pramodgunake@gmail.com>
2021-05-14 23:43:04 +0530

    Life Cycle Hooks, ContentChild implementation

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -27,6 +27,16 @@ export class AppComponent {
     });
   }
 
+  onChangeFirst()
+  {
+    this.serverElements[0].name = "Changed!";
+  }
+
+  onDestroyFirst()
+  {
+    this.serverElements.splice(0,1);
+  }  
+
 
 
 }
--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,18 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterContentChecked, 
+          AfterContentInit, 
+          AfterViewChecked, 
+          AfterViewInit, 
+          Component, 
+          ContentChild, 
+          DoCheck, 
+          ElementRef, 
+          Input, 
+          OnChanges, 
+          OnDestroy, 
+          OnInit, 
+          SimpleChanges, 
+          ViewChild, 
+          ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +20,58 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, 
+    OnChanges, 
+    DoCheck, 
+    AfterContentInit, 
+    AfterContentChecked,
+    AfterViewChecked,
+    AfterViewInit,
+    OnDestroy {
   @Input('srvElement') element : {type: string, name: string, content: string};
+  @Input() name : string;
+  @ViewChild('panelHeader') headerElement: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
+    console.log("constructor called!");
   }
 
+  ngOnInit(): void {
+    console.log("ngOnInit called!");
+    console.log("Header Text: " + this.headerElement);
+    console.log("Paragraph Text: " + this.paragraph);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("ngOnChanges called!");
+    console.log(changes);
+  }
+
+  ngDoCheck()
+  {
+    console.log("ngDoCheck called!");
+  }
+
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit called!")
+    console.log("Paragraph Text: " + this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterContentChecked() {
+    console.log("ngAfterContentChecked called!")
+    }
+
+    ngAfterViewChecked() {
+      console.log("ngAfterViewChecked called!")
+    }
+
+    ngAfterViewInit() {
+      console.log("ngAfterViewInit called!")
+      console.log("Header Text: " + this.headerElement.nativeElement.textContent);
+    }
+
+    ngOnDestroy() {
+      console.log("ngOnDestroy called!")
+    }
 }
